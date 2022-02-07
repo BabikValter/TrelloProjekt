5d3e8635fe96942721539a02c51de80e01ee6352
BabikValter <babik.valter@njit.hu>
2022-02-07 20:29:41 +0100

    localstorage

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,6 @@
 function urlSet() {
     let key = document.getElementById('APIKey').value;
-    let url = "https://api.trello.com/1/authorize?return_url=http://127.0.0.1:5500/boards.html&scope=read,write,account&expiration=1day&name=Trello_API_Projektmunka&key=" + key + "&response_type=token";
+    let url = "https://api.trello.com/1/authorize?return_url=http://127.0.0.1:5500/boards.html&scope=read,write,account&expiration=1hour&name=Trello_API_Projektmunka&key=" + key + "&response_type=token";
+    window.localStorage.setItem('key', key);
     document.getElementById('loginButton').href = url;
 }
